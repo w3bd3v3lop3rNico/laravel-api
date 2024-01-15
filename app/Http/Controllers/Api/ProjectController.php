98132ec1a3c88c100db4eb269c8ce72e063b7ed6
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-15 15:10:09 +0100

    Add API project controller Add route in api.php file

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index(Request $request)
+    {
+        // $results = Project::with('technologies')->paginate(20);
+        $results = Project::all();
+
+        return response()->json([
+            'results' => $results,
+            'success' => true
+        ]);
+    }
+
+}
