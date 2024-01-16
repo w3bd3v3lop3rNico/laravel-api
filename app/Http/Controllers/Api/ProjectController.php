5622614981575a6ad42061f6a34b068f615fcb75
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-16 19:08:18 +0100

    Add show api route Add show controller function

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,12 +10,21 @@ class ProjectController extends Controller
 {
     public function index(Request $request)
     {
-        // $results = Project::with('technologies')->paginate(20);
-        $results = Project::all();
+        $results = Project::with('technologies')->paginate(20);
+        // $results = Project::all();
 
         return response()->json([
             'results' => $results,
             'success' => true
+        ]);
+    }
+
+    public function show(Project $project)
+    {
+        $project->load('technologies');
+
+        return response()->json([
+            'project' => $project
         ]);
     }
 
